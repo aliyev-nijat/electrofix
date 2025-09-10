406b0a2044bd45d2d6cf8feae14d2c4dffb7a832
arvilo <nicat3699@gmail.com>
2025-09-11 00:19:28 +0400

    initial commit

---
+++ src/test/java/az/aliyev_nijat/electrofix/ElectrofixApplicationTests.java
@@ -0,0 +1,13 @@
+package az.aliyev_nijat.electrofix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectrofixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
